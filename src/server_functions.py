5abb7366f49b14aca00caa0661d11d48995e2046
Eduardo Santana <ezambomsantana@gmail.com>
2019-11-08 12:13:54 -0300

    faixas

--- src/server_functions.py
+++ src/server_functions.py
@@ -5,6 +5,7 @@ import pandas as pd
 import csv
 import unidecode
 import math
+import shapefile
 import utm
 from shapely.geometry import shape, LineString, Polygon
 
@@ -22,3 +23,36 @@ def load_acidentes(tipos):
         acidentes_copy = acidentes_copy[acidentes_copy['Tipo de Acidente'] == tipos[0].encode('utf-8')] 
 
     return acidentes_copy
+
+def load_corredores():
+    corredores = gpd.GeoDataFrame.from_file("../corredores/corredores/SIRGAS_SHP_corredoronibus.shp", encoding='latin-1')
+    corredores.crs = {'init' :'epsg:22523'}
+    corredores = corredores.to_crs({"init": "epsg:4326"})
+    return corredores
+
+def load_faixas():
+    sf = shapefile.Reader("../corredores/faixas/SAD69_faixa_onibus.shp", encoding='latin-1')
+    fields = [x[0] for x in sf.fields][1:]
+    print(fields)
+    records =[list(i) for i in sf.records()]
+    shps = [s.points for s in sf.shapes()]
+
+    #write into a dataframe
+    df = pd.DataFrame(columns=fields, data=records)
+    df = df.assign(coords=shps)
+
+    print(df)
+
+    lines = []
+    nomes = []
+    for index, row in df.iterrows():
+        list_coords = row['coords']
+        if len(list_coords) > 1:
+            line = LineString(list_coords)
+            lines.append(line)
+            nomes.append(row['nome'])
+    frame = pd.DataFrame(list(zip(lines, nomes)), columns =['geometry', 'FE_VIA'])
+    faixas = gpd.GeoDataFrame(frame)
+    faixas.crs = {'init' :'epsg:22523'}
+    faixas = faixas.to_crs({"init": "epsg:4326"})
+    return faixas
