0d3f2f4ad3e941af4af4f7d948fa40c59539ce1d
Eduardo Santana <ezambomsantana@gmail.com>
2019-11-10 01:21:22 -0300

    distritos

--- src/zonas.py
+++ src/zonas.py
@@ -26,21 +26,21 @@ def point_from_lat_long(linha):
     except:
         return None
 
-radares = pd.read_csv('radares.csv')
+radares = pd.read_csv('radares_moto.csv')
 radares = radares[radares['contagem'] > 0]
 pontos_radares = radares.latitude_l.apply(lambda l: point_from_tuple_str(l))
 radares_geodf = gpd.GeoDataFrame(data=radares, geometry=pontos_radares, crs=crs)
 radares_geodf = radares_geodf[~radares_geodf.geometry.isnull()]
 
 acidentes = gpd.GeoDataFrame.from_file("../incidentes/SIRGAS_SHP_acidentecet/SIRGAS_SHP_acidentecet.shp", encoding='latin-1')
+acidentes = acidentes[acidentes['aci_data'].str.contains('2018', regex=True)]
 acidentes.crs = {'init' :'epsg:22523'}
 acidentes = acidentes.to_crs({"init": "epsg:4326"})
 acidentes_geodf = acidentes[~acidentes.geometry.isnull()]
 
-zonas = gpd.GeoDataFrame.from_file("../data/shapes/Zonas_2017_region.shp", encoding='latin-1')
+zonas = gpd.GeoDataFrame.from_file("../data/shapes/Distritos_2017_region.shp", encoding='latin-1')
 zonas = zonas.to_crs({"init": "epsg:4326"}) 
 zonas['NomeDistri'] = zonas['NomeDistri'].apply(lambda x: unidecode.unidecode(x))
-zonas['NomeZona'] = zonas['NomeZona'].apply(lambda x: unidecode.unidecode(x))
 
 sjoin = gpd.sjoin(zonas, radares_geodf, op='contains')
 sjoin_zonas = gpd.sjoin(zonas, acidentes_geodf, op='contains')
@@ -48,16 +48,14 @@ sjoin_zonas = gpd.sjoin(zonas, acidentes_geodf, op='contains')
 print(sjoin)
 print(sjoin_zonas)
 
-
-count = sjoin.groupby('NomeZona', as_index=True).agg({'contagem': 'sum', 'autuacoes': 'sum'})
-count2 = sjoin_zonas.groupby('NomeZona', as_index=True).agg({'NomeDistri': 'count'})
-
+count = sjoin.groupby('NomeDistri', as_index=True).agg({'cont_motos': 'sum', 'cont_carros': 'sum', 'cont_onibus': 'sum', 'cont_caminhao': 'sum'})
+count2 = sjoin_zonas.groupby('NomeDistri', as_index=True).agg({'NomeDistri': 'count'})
+count2.columns = ['acidentes']
 
 final = count.join(count2)
+print(final)
 
-
-final['media'] = final['autuacoes']  / final['contagem'] * 100
 final = final.fillna(0)
-print(final.sort_values(by='NomeDistri', ascending=False))
+print(final.sort_values(by='acidentes', ascending=False))
 
 print(final.corr())
